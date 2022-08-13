9eab582b914e0fe70fd1d461444bb7222dc8be49
Wesley Chigbu <chigbuw@yahoo.com>
2022-08-13 04:55:38 +0100

    stats

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const menu = document.getElementById('mobile-menu');
+const counters = document.querySelectorAll('.counter');
 
 btn.addEventListener('click', navToggle);
 
@@ -9,4 +10,26 @@ function navToggle(){
     overlay.classList.toggle('overlay-show');
     document.body.classList.toggle('stop-scrolling');
     menu.classList.toggle('show-menu');
+}
+
+function countUp(){
+    counters.forEach((counter) => {
+        counter.innerText = '0';
+
+        const updateCounter = () => {
+            //Get count target
+            const target = +counter.getAttribute('data-target');
+            //Get current counter value
+            const c = +counter.innerText;
+
+            //Create increment
+            const increment = target / 100;
+
+            // If counter is less than target, add increment
+            if (c < target){
+                counter.innerText = `${Math.ceil(c + increment)}`;
+            }
+        };
+        updateCounter();
+    });
 }
