962100c3d126e8c882f7501710b40a633295c249
Wesley Chigbu <chigbuw@yahoo.com>
2022-08-23 09:46:09 +0100

    falcon 9 fix

--- JS/script.js
+++ JS/script.js
@@ -2,14 +2,28 @@ const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const menu = document.getElementById('mobile-menu');
 const counters = document.querySelectorAll('.counter');
+let scrollStarted = false;
 
 btn.addEventListener('click', navToggle);
+document.addEventListener('scroll', scrollPage);
 
 function navToggle() {
     btn.classList.toggle('open');
     overlay.classList.toggle('overlay-show');
     document.body.classList.toggle('stop-scrolling');
     menu.classList.toggle('show-menu');
+}
+
+function scrollPage(){
+  const scrollPos = window.scrollY;
+  if(scrollPos > 90 && !scrollStarted){
+    countUp();
+    scrollStarted = true;
+  }
+  else if (scrollPos < 90 && scrollStarted){
+    reset();
+    scrollStarted = false;
+  }
 }
 
 function countUp() {
@@ -28,6 +42,8 @@ function countUp() {
                 counter.innerText = `${Math.ceil(c + increment)}`;
 
                 setTimeout(updateCounter, 75);
+            } else{
+              counter.innerText = target;
             }
 
         };
@@ -35,4 +51,9 @@ function countUp() {
     });
 }
 
-countUp()
+function reset(){
+  counters.forEach((counter) => (counter.innerHTML = '0')
+  );
+}
+
+scrollPage();
